7ef1960f80139aac0e01157f74506c0d49f47875
Sahil Kale <sahil.kale1@gmail.com>
2025-01-17 10:26:37 -0500

    Fix sector convention

--- foc-control-lib/test/test_6step.cpp
+++ foc-control-lib/test/test_6step.cpp
@@ -34,17 +34,25 @@ void check_phase_command_correct(six_step_phase_control_action_E control_action,
 
 TEST(SixStep, SectorLookup) {
     // Test the sector lookup table
-    CHECK_EQUAL(0, six_step_get_sector(DEG_TO_RAD(30.0f)));
-    CHECK_EQUAL(1, six_step_get_sector(DEG_TO_RAD(90.0f)));
-    CHECK_EQUAL(2, six_step_get_sector(DEG_TO_RAD(150.0f)));
-    CHECK_EQUAL(3, six_step_get_sector(DEG_TO_RAD(210.0f)));
-    CHECK_EQUAL(4, six_step_get_sector(DEG_TO_RAD(270.0f)));
-    CHECK_EQUAL(5, six_step_get_sector(DEG_TO_RAD(330.0f)));
+    CHECK_EQUAL(0, six_step_get_sector(DEG_TO_RAD(0.0f)));
+    CHECK_EQUAL(0, six_step_get_sector(DEG_TO_RAD(29.9f)));
+    CHECK_EQUAL(1, six_step_get_sector(DEG_TO_RAD(30.0f)));
+    CHECK_EQUAL(1, six_step_get_sector(DEG_TO_RAD(89.9f)));
+    CHECK_EQUAL(2, six_step_get_sector(DEG_TO_RAD(90.0f)));
+    CHECK_EQUAL(2, six_step_get_sector(DEG_TO_RAD(149.9f)));
+    CHECK_EQUAL(3, six_step_get_sector(DEG_TO_RAD(150.0f)));
+    CHECK_EQUAL(3, six_step_get_sector(DEG_TO_RAD(209.9f)));
+    CHECK_EQUAL(4, six_step_get_sector(DEG_TO_RAD(210.0f)));
+    CHECK_EQUAL(4, six_step_get_sector(DEG_TO_RAD(269.9f)));
+    CHECK_EQUAL(5, six_step_get_sector(DEG_TO_RAD(270.0f)));
+    CHECK_EQUAL(5, six_step_get_sector(DEG_TO_RAD(329.9f)));
+    CHECK_EQUAL(0, six_step_get_sector(DEG_TO_RAD(330.0f)));
+    CHECK_EQUAL(0, six_step_get_sector(DEG_TO_RAD(359.0f)));
 }
 
 TEST(SixStep, SixStepSectorDutyCycleSanity) {
     for (uint8_t i = 0; i < SIX_STEP_NUM_SECTORS; i++) {
-        const float test_angle = DEG_TO_RAD(60.0f * i) + DEG_TO_RAD(30.0f);
+        const float test_angle = DEG_TO_RAD(60.0f * i);
 
         six_step_duty_cycles_t six_step_data = six_step_get_duty_cycle(test_angle, 0.5f);
         six_step_sector_control_t six_step_positive_control = six_step_sector_positive_control_table[i];
